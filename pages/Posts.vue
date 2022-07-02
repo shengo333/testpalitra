22c9458a54c1a03a2258eb7b08dc3b5f5f02dcf5
shengo333 <BEKASHENGELIA333@GMAIL.COM>
2022-07-02 15:04:59 +0400

    most changes

--- pages/Posts.vue
+++ pages/Posts.vue
@@ -1,18 +1,83 @@
 <template>
     <div>
         <Header />
-        <h1>here go posts</h1>
+        <div class="posts">
+            <div class="post" v-for="post in posts" :key="post.id">
+                <h2>{{post.title}}</h2>
+                <p>{{post.body}}</p>
+
+                <li>
+                    {{getUserNameById(post.userId)}}
+                </li>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
 import Header from './Header.vue'
-
 export default {
-    components:{Header}
+
+    components: {Header},
+    data(){
+        return{
+            urlPosts: 'https://jsonplaceholder.typicode.com/posts',
+            urlUsers: 'https://jsonplaceholder.typicode.com/users',
+            posts: [],
+            users:[],
+            userName:'',
+        }
+    },
+    mounted(){
+        this.getData();
+        this.getUsers();
+    },
+    methods:{
+        async getData(){
+            const response = await axios.get(this.urlPosts);
+
+            console.log(response);
+            this.posts = response.data;
+        },
+        async getUsers(){
+            const response = await axios.get(this.urlUsers);
+            console.log(response);
+            this.users = response.data;
+            console.log(this.users)
+        },
+        getUserNameById(postUserId) {
+            let foundUser = this.users.find(user => user.id == postUserId)
+            if(foundUser){
+                return foundUser.name
+            }
+        }
+    }
 }
 </script>
 
 <style>
+body{
+    margin: 0;
+}
+
+.page-enter-active,
+.page-leave-active {
+  transition: opacity 0.5s;
+}
+.page-enter,
+.page-leave-to {
+  opacity: 0;
+}
+
+.post{
+    width: 80%;
+    padding: 1rem;
+    margin-left: auto;
+    margin-right: auto;
+    border: 1px solid gray;
+    margin: 1rem auto;
+    background: rgb(235, 232, 232);
+}
 
 </style>
--- pages/Users.vue
+++ pages/Users.vue
@@ -2,16 +2,71 @@
     <div>
         <Header />
         <h1>Here go users</h1>
+        <div class="users">
+            <div class="user" v-for="user in users" :key="user.userId">
+                <h1>{{user.id}}</h1>
+                <h1>{{user.name}}</h1>
+                <h3>{{user.username}}</h3>
+                <p>{{user.email}}</p>
+                <p>{{user.address.street}}</p>
+                <p>{{user.address.suite}}</p>
+                <p>{{user.address.city}}</p>
+                <p>{{user.address.zipcode}}</p>
+                <p>{{user.address.geo.lat}}{{user.address.geo.lng}}</p> 
+                <ul>
+                    <li v-for="userPost in getPostlistByUserId(user.id)" :key="userPost.id">
+                        {{userPost.title}}
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
 import Header from './Header.vue'
-
 export default {
-    components:{Header}
+// get deta from server
+    components: {Header},
+    data(){
+        return{
+            urlPosts: 'https://jsonplaceholder.typicode.com/posts',
+            urlUsers: 'https://jsonplaceholder.typicode.com/users',
+            posts: [],
+            users:[],
+            userName:'',
+            userPosts:[],
+        }
+    },
+    mounted(){
+        this.getData();
+        this.getUsers();
+
+    },
+    methods:{
+
+            async getData(){
+                const response = await axios.get(this.urlPosts);
+                console.log(response);
+                this.posts = response.data;
+            },
+            async getUsers(){
+                const response = await axios.get(this.urlUsers);
+                console.log(response);
+                this.users = response.data;
+            },
+            getPostlistByUserId(userId) {
+                // let foundUser = this.users.find(user => user.id == postUserId)
+                // this.userName = foundUser.name;
+
+                let filteredPosts = this.posts.filter(post => post.userId == userId)
+                return filteredPosts
+            }
+    },
 }
 </script>
+
 
 <style>
 
